436fa9a5a0012447db869ddea0704869dbea5ca6
Danilo Barrinha <dbarrinha@hotmail.com>
2025-01-21 20:15:30 -0300

    feat: enhance accessibility guidelines and practices in Accessibility component

--- src/Accessibility/Accessibility.tsx
+++ src/Accessibility/Accessibility.tsx
@@ -12,4 +12,17 @@ const Accessibility = () => {
 //REMOVER ELEMENTOS DESNECESSÁRIOS DO TABINDEX USANDO tabindex="-1"
 
 //usar aria-label para descrever elementos interativos que não possuem texto como botões e tags a (anchor)
+
+
+
+//FOLLOWING WCAG (Web Content Accessibility Guidelines) STANDARDS
+//USING NVDA SCREEN READER to map the problems
+//USING AXE DEVTOOLS TO IDENTIFY ACCESSIBILITY IMPROVEMENT POINTS
+//USE SEMANTIC HTML
+//ADD ALTERNATIVE TEXT TO IMAGE TAGS
+//NAVIGATE BETWEEN ELEMENTS USING THE TABINDEX ATTRIBUTE
+//use tabindex='0' to maintain the natural order of the document
+//REMOVE UNNECESSARY ELEMENTS FROM TABINDEX USING tabindex="-1"
+
+//use aria-label to describe interactive elements that do not have text such as buttons and a (anchor) tags
 export default Accessibility
